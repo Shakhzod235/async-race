8a1caa1c512be712f52ece4c25f2c79b40087a5e
Shakhzod235 <shakhzod235@mail.ru>
2023-07-25 16:30:19 +0500

    feat: add html and css to webpack

--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -1,19 +1,27 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   mode: 'development',
   entry: './src/main.ts',
   output: {
-  path: path.resolve('./', 'public'),
-  filename: 'bundle.js',
+    path: path.resolve('./', 'public'),
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./', 'public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
 };
