6156fe1c572fd68a6d905935dc74fdd390231dee
Shakhzod235 <shakhzod235@mail.ru>
2023-07-26 15:54:06 +0500

    refactor: move types to a separate module

--- async-race/src/main.ts
+++ async-race/src/main.ts
@@ -1,17 +1,1 @@
-export type carType = {
-  id: number,
-  name: string,
-  color: string,
-  isEngineActive?: boolean
-}
 
-export type newCarType = {
-  name: string,
-  color: string
-}
-
-export type winnerType = {
-  id: number,
-  wins: number,
-  time: number
-}
